5ce0d87ed7de07a39824ea9ab983a3c158b306be
danielxwang90 <daniel.wang@oliverwyman.com>
2022-07-27 23:54:56 -0400

    version 0.0.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('metadata.json') as fr:
 
 setuptools.setup(
     name="gryphon-model-plotting-regression", 
-    version="0.0.1",
+    version="0.0.2",
     author=metadata.get("author", ""),
     author_email=metadata.get("author_email", ""),
     description=metadata.get("description", ""),
